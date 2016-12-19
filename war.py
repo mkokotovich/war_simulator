e9053cc44afbdedfc5cae5c49718a116adc73cc6
Matthew Kokotovich <mkokotovich@gmail.com>
2016-12-19 11:47:03 -0600

    Added Trick class to manage each trick, game simulation is complete

--- war.py
+++ war.py
@@ -20,11 +20,13 @@ class Player:
         self.hand += initial_cards
 
     def print_cards(self, debug=False):
-        msg = "Player {} hand: Aces: {}  Total: {}".format(self.name, len(self.hand.find('Ace')), len(self.hand))
+        hand_aces = 0 if self.hand.size == 0 else len(self.hand.find('Ace'))
+        pile_aces = 0 if self.pile.size == 0 else len(self.pile.find('Ace'))
+        msg = "Player {} hand: Aces: {}  Total: {}".format(self.name, hand_aces, len(self.hand))
         msg += "\n"
         if debug:
             msg += str(self.hand)
-        msg += "Player {} pile: Aces: {}  Total: {}".format(self.name, len(self.pile.find('Ace')), len(self.pile))
+        msg += "Player {} pile: Aces: {}  Total: {}".format(self.name, pile_aces, len(self.pile))
         if debug:
             msg += "\n"
             msg += str(self.pile)
@@ -32,8 +34,8 @@ class Player:
 
     # Returns a single card
     def play_card(self):
-        if len(self.hand.cards) == 0:
-            if len(self.pile.cards) == 0:
+        if self.hand.size == 0:
+            if self.pile.size == 0:
                 # Game over
                 return None
             else:
@@ -41,9 +43,17 @@ class Player:
                 self.hand.shuffle()
         return self.hand.deal(1)[0]
 
-    def still_playing(self):
-        # Work around bug in pydealer where size() on empty Stack fails
-        return len(self.hand.cards) != 0 or len(self.pile.cards) != 0
+    def has_cards(self):
+        return self.hand.size != 0 or self.pile.size != 0
+
+    def how_many_cards_can_be_played_for_war(self):
+        total_cards = self.hand.size + self.pile.size
+        war_cards = 3
+        if total_cards < 4:
+            war_cards = total_cards - 1
+        if total_cards == 0:
+            war_cards = 0
+        return war_cards
 
     def add_cards_to_pile(self, cards):
         self.pile += cards
@@ -52,12 +62,59 @@ class Player:
         return self.print_cards(False)
 
 
+class Trick:
+    def __init__(self):
+        self.cards = {}
+        self.bonus_cards = []
+        self.current_winners = {}
+        self.current_highest_card = None
+
+    def update_winners_if_needed(self, player_id, card):
+        if self.current_highest_card == None or self.current_highest_card.lt(card, POKER_RANKS["values"]):
+            self.current_highest_card = card
+            self.current_winners = {}
+        if self.current_highest_card is not None and self.current_highest_card.eq(card, POKER_RANKS["values"]):
+            # Add to winners
+            self.current_winners[player_id] = card
+
+    def add_primary_card(self, player_id, card):
+        if card is not None:
+            self.cards[player_id] = card
+            self.update_winners_if_needed(player_id, card)
+
+    def add_bonus_card(self, card):
+        if card is not None:
+            self.bonus_cards.append(card)
+
+    def is_war(self):
+        return len(self.current_winners) > 1
+
+    def prepare_for_war(self):
+        self.bonus_cards += self.cards.values()
+        self.cards = {}
+        self.current_winners = {}
+        self.current_highest_card = None
+
+    def get_winner_id(self):
+        return self.current_winners.keys()[0]
+
+    def get_all_cards_as_stack(self):
+        stack = pydealer.Stack()
+        for x in self.cards.values():
+            stack += [x]
+        for x in self.bonus_cards:
+            stack += [x]
+        return stack
+
+
+
 class WarGameManager:
     def __init__(self, num_players=2):
         self.num_players = num_players
         self.players = {}
         self.deck = pydealer.Deck(ranks=POKER_RANKS)
         self.initialize_players()
+        self.number_of_hands = 0
 
     def initialize_players(self):
         for i in range(0, self.num_players):
@@ -71,14 +128,22 @@ class WarGameManager:
 
     def reset_game(self):
         self.initialize_players()
+        self.number_of_hands = 0
         self.deal_new_deck()
 
     def is_game_over(self):
         for i in range(0, self.num_players):
-            if not self.players[i].still_playing():
+            if not self.players[i].has_cards():
                 return True
         return False
 
+    def game_summary(self):
+        msg = ""
+        for i in range(0, self.num_players):
+            if self.players[i].has_cards():
+                msg +=  "{} won after {} hands".format(self.players[i].name, self.number_of_hands)
+                break
+        return msg
 
     def __str__(self):
         msg = ""
@@ -87,28 +152,36 @@ class WarGameManager:
         return msg
 
     def play_hand(self, debug=False):
-        hand = {}
+        self.number_of_hands += 1
+        trick = Trick()
         for i in range(0, self.num_players):
-            hand[i] = self.players[i].play_card()
+            card = self.players[i].play_card()
+            trick.add_primary_card(i, card)
             if debug:
-                print "{}: {}".format(self.players[i].name, str(hand[i]))
-        winning_card = max(hand.values())
-        winners = {}
-        for i in range(0, self.num_players):
-            if hand[i] == winning_card:
-                winners[i] = winning_card
-        if len(winners) == 1:
-            # One winner, no war
-            winner_id = winners.keys()[0]
-            cards = pydealer.Stack()
-            for x in hand.values():
-                cards += [x]
-            self.players[winner_id].add_cards_to_pile(cards)
-        else:
-            # WAR
-            # TODO: make war logic
-            winner_id = winners.keys()[0]
-            self.players[winner_id].add_cards_to_pile(list(hand.values()))
+                print "{}: {}".format(self.players[i].name, str(card))
+        while trick.is_war():
+            trick.prepare_for_war()
+            cards_for_war_list = []
+            for i in range(0, self.num_players):
+                cards_for_war_list.append(self.players[i].how_many_cards_can_be_played_for_war())
+            cards_for_war = min(cards_for_war_list)
+            if debug:
+                print "Warring with {} bonus cards".format(cards_for_war)
+            for i in range(0, self.num_players):
+                for j in range(0, cards_for_war):
+                    card = self.players[i].play_card()
+                    trick.add_bonus_card(card)
+                    if debug:
+                        print "{} added bonus card: {}".format(self.players[i].name, str(card))
+                card = self.players[i].play_card()
+                trick.add_primary_card(i, card)
+                if debug:
+                    print "{} war card: {}".format(self.players[i].name, str(card))
+        # Give all cards to winner
+        cards = trick.get_all_cards_as_stack()
+        self.players[trick.get_winner_id()].add_cards_to_pile(cards)
+        if debug:
+            print "{} won {} cards".format(self.players[trick.get_winner_id()].name, cards.size)
 
 
 def main():
@@ -118,6 +191,7 @@ def main():
     while not war.is_game_over():
         war.play_hand(debug=True)
         print war
+    print war.game_summary()
 
 if __name__ == "__main__":
     main()
